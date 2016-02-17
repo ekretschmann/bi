c2d97502729e790214c703972f07f5d1119670f0
Ernst Kretschmann <ernst.kretschmann@gmail.com>
2016-02-17 08:28:14 +0000

    start with from - to input

--- public/modules/core/controllers/planner.client.controller.js
+++ public/modules/core/controllers/planner.client.controller.js
@@ -9,21 +9,38 @@ angular.module('core').controller('PlannerController',
 
 
             $scope.init = function () {
-                $scope.from = '120 Cherry Road';
-                $scope.to = '48 Chesterton Road';
+                $scope.from = 'Current Location';
+                $scope.to = undefined;
                 $scope.routePlannerExpanded = false;
                 $scope.fromInput = false;
                 $scope.fromInputFocus = false;
                 $scope.toInput = false;
                 $scope.priceSlider = 10;
-                $scope.startTime = $scope.getDateString(Date.now());
+                $scope.startDate = $scope.getStartDate(Date.now());
+                $scope.startTime = $scope.getStartTime(Date.now());
+
+                $scope.locationInputState = 'init';
             };
 
-            $scope.getDateString = function(d) {
+            $scope.getStartDate = function(d) {
                 var result = '';
                 result += new Date(d).getDate() +'/';
-                result += new Date(d).getMonth()+'/';
-                result += new Date(d).getFullYear()+' ';
+                result += ((new Date(d).getMonth())+1)+'/';
+                result += ((new Date(d).getFullYear())-2000)+' ';
+                //if (new Date(d).getHours()<10) {
+                //    result += '0';
+                //}
+                //result += new Date(d).getHours()+':';
+                //if (new Date(d).getMinutes()<10) {
+                //    result += '0';
+                //}
+                //result += new Date(d).getMinutes();
+                return result;
+            };
+
+            $scope.getStartTime = function(d) {
+                var result = '';
+
                 if (new Date(d).getHours()<10) {
                     result += '0';
                 }
@@ -35,6 +52,20 @@ angular.module('core').controller('PlannerController',
                 return result;
             };
 
+
+            $scope.changeDate = function(howMuch) {
+
+                $scope.slider.value += howMuch * 1000 * 60 * 60 * 24;
+                $scope.startTime = $scope.getStartTime( $scope.slider.value);
+                $scope.startDate = $scope.getStartDate( $scope.slider.value);
+            };
+
+            $scope.changeTime = function(howMuch) {
+
+                $scope.slider.value += howMuch * 1000 * 60;
+                $scope.startTime = $scope.getStartTime( $scope.slider.value);
+                $scope.startDate = $scope.getStartDate( $scope.slider.value);
+            };
 
             $scope.slider = {
                value: Date.now(),
@@ -53,7 +84,8 @@ angular.module('core').controller('PlannerController',
                         return '';
                     },
                     onChange: function(sliderId, modelValue, highValue) {
-                        $scope.startTime = $scope.getDateString(modelValue);
+                        $scope.startTime = $scope.getStartTime(modelValue);
+                        $scope.startDate = $scope.getStartDate(modelValue);
                     }
 
                 }
@@ -84,6 +116,48 @@ angular.module('core').controller('PlannerController',
                 });
             };
 
+            $scope.selectFrom = function() {
+                $scope.locationInputState = 'select-from';
+            };
+
+            $scope.selectTo = function() {
+                $scope.locationInputState = 'select-to';
+            };
+
+            $scope.$on('leafletDirectiveMap.click', function(event, args){
+                var leafEvent = args.leafletEvent;
+
+                console.log($scope.locationInputState);
+                if ($scope.locationInputState === 'select-from') {
+                    $scope.markers.push({
+                        lat: leafEvent.latlng.lat,
+                        lng: leafEvent.latlng.lng,
+                        icon: {
+                            type: 'div',
+                            html: '<div class="bus-icon"></div>',
+                            className: 'map-marker bus-icon'
+                        },
+                        message: 'From here'
+                    });
+                    $scope.locationInputState = 'init';
+                }
+
+                if ($scope.locationInputState === 'select-to') {
+
+                    $scope.markers.push({
+                        lat: leafEvent.latlng.lat,
+                        lng: leafEvent.latlng.lng,
+                        icon: {
+                            type: 'div',
+                            html: '<div class="bus-icon"></div>',
+                            className: 'map-marker bus-icon'
+                        },
+                        message: 'To here'
+                    });
+                    $scope.locationInputState = 'init';
+                }
+            });
+
             angular.extend($scope, {
 
                 mk: {
@@ -92,8 +166,9 @@ angular.module('core').controller('PlannerController',
                     lng: -0.7532501220703125,
                     zoom: 12
                 },
+                events: {},
                 paths: {},
-                markers: {},
+                markers: [],
                 defaultIcon: {},
                 defaults: {
                     scrollWheelZoom: false
